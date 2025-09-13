61e5b5b6fb45c0cf8e56d76c333c4912414b4cae
MohamedMahmouddev <modyseal@gmail.com>
2025-09-13 15:43:27 +0300

    basic operation file uplode

---
+++ python_introduction/basic-operation.py
@@ -0,0 +1,5 @@
+numper1 = 10
+numper2 = 5
+print(f"Addition of 10 and 5 is {numper1+numper2}")
+print(f"Subtraction of 10 and 5 is {numper1-numper2}")
+print(f"Multiplication of 10 and 5 is {numper1*numper2}")
