fea6c34fe24d8aa12d2d8a0d8d99c08689205596
MohamedMahmouddev <modyseal@gmail.com>
2025-10-11 17:32:11 +0300

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,7 +7,6 @@ class BankAccount:
     def deposit(self, amount):
         self.account_balance += amount
         print(f"Deposited: {amount}")
-        print(f"Current balance: {self.account_balance}")
 
     def withdraw(self, amount):
         if self.account_balance >= amount:
